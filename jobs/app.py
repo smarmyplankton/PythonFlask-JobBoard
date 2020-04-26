1fef4a248d5a4491cfa1ad2bfe5dac7f90d49af0
brian Gray <gray4444@gmail.com>
2020-04-26 19:17:41 -0400

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -28,12 +28,12 @@ def close_connection(exception):
     if connection != None:
         connection.close()
 
-def job():
-    return render_template('job.html')
-
-
 @app.route("/")
 @app.route("/jobs")
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
+
+@app.route("/job/<job_id>")
+def job():
+    return render_template('job.html')
