982650635c83c20fcdbd5fe337543a4bb50ef02d
brian Gray <gray4444@gmail.com>
2020-04-26 20:13:56 -0400

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, redirect, url_for
 import sqlite3, datetime
 
 application = app = Flask(__name__)
